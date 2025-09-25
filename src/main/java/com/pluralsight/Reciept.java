57589da74c66117674ec541a28ca5154375abdaa
lauramunozn0 <lauramunozlaw@gmail.com>
2025-09-25 02:03:14 -0400

    created a new class

---
+++ src/main/java/com/pluralsight/Reciept.java
@@ -0,0 +1,26 @@
+package com.pluralsight;
+
+import java.net.SocketAddress;
+
+public class Reciept {
+    public static void main(String[] args) {
+        //Step 1
+String item = "Bag";
+double price = 100.00;
+        System.out.println("the price of the " + item + " is " + price );
+
+//Step 2
+int firstNumber = 6;
+        System.out.println("created the number " + firstNumber);
+
+// Step 3
+int quantityApples = 3;
+String items = "apples";
+double priceApples = 7.10;
+        System.out.println("I bought " + quantityApples + " " + items + " for the price of " + priceApples );
+// I learned that adding just " " to add space between the words.
+
+
+    }
+}
+
--- src/main/java/com/pluralsight/VariableApp.java
+++ src/main/java/com/pluralsight/VariableApp.java
@@ -2,6 +2,36 @@ package com.pluralsight;
 
 public class VariableApp {
     public static void main(String[] args) {
+//step 1
+String favColor = "gold";
+        System.out.println("my favorite color is " + favColor);
+
+int yearUpStar = 2025;
+        System.out.println("I started year up united " + yearUpStar);
+
+char middleInitial = 'N';
+        System.out.println("my middle initial is " + middleInitial);
+
+boolean hasPets = true;
+        System.out.println("Do you have pets? " + hasPets);
+
+String niceMessage = "Be happy";
+        System.out.println("Nice message for you " + niceMessage);
+
+ //Step 2
+int daysWeek = 7;
+        System.out.println("We have " + daysWeek + " days a week" );
+
+ double coffePrice = 4.99;
+        System.out.println("the price of coffe is " + coffePrice);
+
+ char favLetter = 'L';
+        System.out.println("My favorite letter is " + favLetter);
+
+  boolean isRaining = true;
+        System.out.println("Is it raining? " + isRaining);
+
+
 
     }
 }
